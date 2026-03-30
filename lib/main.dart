5e35a72f72dfa8805d409f2a047fba2e6268aa32
keerthan-rao <keerthanrao8@gmail.com>
2026-03-30 12:55:51 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Investments(),
+      home: Payments(),
     );
   }
 } 
